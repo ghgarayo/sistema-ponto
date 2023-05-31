7ef6e0385640c66454cde1179a30f1d116c461b1
Gustavo Garayo <gustavo.garayo@kron.digital>
2023-05-31 12:00:28 -0300

    alteracao retorno json registro de pontos

--- src/main/java/com/lbe/sistemaponto/domain/ponto/DadosListagemPonto.java
+++ src/main/java/com/lbe/sistemaponto/domain/ponto/DadosListagemPonto.java
@@ -3,14 +3,14 @@ package com.lbe.sistemaponto.domain.ponto;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record DadosListagemPonto(LocalDate dataCompleta, String login,
+public record DadosListagemPonto(LocalDate dataCompleta,
     LocalTime horarioEntrada1, String latitudeEntrada1, String longitudeEntrada1,
     LocalTime horarioSaida1, String latitudeSaida1, String longitudeSaida1,
     LocalTime horarioEntrada2, String latitudeEntrada2, String longitudeEntrada2,
     LocalTime horarioSaida2, String latitudeSaida2, String longitudeSaida2) {
 
     public DadosListagemPonto(Ponto ponto){
-      this(ponto.getDataCompleta() ,ponto.getLogin(),
+      this(ponto.getDataCompleta(),
       ponto.getHorarioEntrada1(), ponto.getLatitudeEntrada1(), ponto.getLongitudeEntrada1(),
       ponto.getHorarioSaida1(), ponto.getLatitudeSaida1(), ponto.getLongitudeSaida1(),
       ponto.getHorarioEntrada2(), ponto.getLatitudeEntrada2(), ponto.getLongitudeEntrada2(),
